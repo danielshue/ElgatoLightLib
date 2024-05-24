1c065e108f24d38446b7c4d839e7bf51e102da46
Dan Shue <danshue@microsoft.com>
2024-05-24 11:36:30 -0700

    Updated project references, code cleanup, and README
    Updated `ElgatoLightLib.csproj` to include references to `LICENSE` and `README.md`.
    
    In `ElgatoLightMgr.cs`, modified `BonjourDiscoveryElgatoKeyLightService` and `AccessoryInfoEndPointTemplate` from `const` to `static readonly`, added remarks for `Elgato EveKey LightService`, removed unused code, and made `RetrieveKeyLightInfoAsync` static.
    
    Enhanced `README.md` with a more detailed description of the `ElgatoKeyLight` library.

--- ElgatoLightLib/ElgatoLightMgr.cs
+++ ElgatoLightLib/ElgatoLightMgr.cs
@@ -30,14 +30,16 @@ namespace ElgatoLightLib
         /// <summary>
         /// Discover Service for the Elgato Key Light.
         /// </summary>
-        private const string BonjourDiscoveryElgatoKeyLightService = "_elg._tcp.local.";
-        //private const string ElgatoEveKeyLightService = "_hap._tcp.local.";
+        /// <remarks>
+        /// Elgato EveKey LightService would be "_hap._tcp.local."
+        /// </remarks>
+        private static readonly string BonjourDiscoveryElgatoKeyLightService = "_elg._tcp.local.";
 
         /// <summary>
         /// Template used for the Elgato Keylight Endpoint.
         /// </summary>
-        private const string AccessoryInfoEndPointTemplate = @"http://{0}:{1}/elgato/accessory-info";
-        
+        private static readonly string AccessoryInfoEndPointTemplate = @"http://{0}:{1}/elgato/accessory-info";
+
         /// <inheritdoc/>
         public async Task<IList<ElgatoLight>> StartDiscoverAsync(int timeout = 30, CancellationToken cancellationToken = default)
         {
@@ -61,7 +63,7 @@ namespace ElgatoLightLib
                 foreach (var resolvedElgatoLightService in elgatoDiscoveredServices)
                 {
                     var light = await RetrieveKeyLightInfoAsync(resolvedElgatoLightService.IPAddress, resolvedElgatoLightService.DisplayName, resolvedElgatoLightService.Services.FirstOrDefault().Value.Port);
-
+                    
                     if(light != null)
                     {
 
@@ -75,18 +77,7 @@ namespace ElgatoLightLib
                 }
             }
 
-/*            if (eveDiscoveredServices != null)
-            {
-                // for each light that is discovered, retrieve details about the keylight by calling the device directly
-                foreach (var resolvedEveLightService in eveDiscoveredServices)
-                {
-                    var light = await RetrieveKeyLightInfoAsync(resolvedEveLightService.IPAddress, resolvedEveLightService.DisplayName, resolvedEveLightService.Services.FirstOrDefault().Value.Port);
-
-                    //lights.Add(light);
-                }
-            }
-
-*/            return lights;
+            return lights;
         }
 
         /// <summary>
@@ -96,7 +87,7 @@ namespace ElgatoLightLib
         /// <param name="name">The display name of the device.</param>
         /// <param name="port">the port number used for communication with the device.</param>
         /// <returns></returns>
-        private async Task<ElgatoLight> RetrieveKeyLightInfoAsync(string ipaddress, string name, int port)
+        private static async Task<ElgatoLight> RetrieveKeyLightInfoAsync(string ipaddress, string name, int port)
         {
             var endPoint = string.Format(AccessoryInfoEndPointTemplate, ipaddress, port);
 
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# ElgatoKeyLight
-C# library for controlling Elgato Lights
+# Elgato Lights
+
+The ElgatoKeyLight library is a C# library specifically designed for controlling Elgato Lights. 
+
+Elgato Lights are programmable LED lights that can be used for various purposes, such as lighting setups for streaming, video production, or general ambiance lighting.
+
+This library provides a set of functions and classes that allow you to interact with and control Elgato Lights programmatically. 
+With this library, you can adjust the brightness, color temperature, and other settings of the lights, as well as turn them on or off.
+
+To use the ElgatoKeyLight library in your C# project, you will need to add a reference to the library and import the 
+necessary namespaces. Once you have done that, you can create instances of the ElgatoKeyLight class and use its methods 
+to control the lights.
+
+It's worth noting that in order to use this library, you will need to have the necessary hardware, such 
+as an Elgato Key Light or Key Light Air, and the Elgato Control Center software installed on your computer.
+
+If you have any specific questions or need help with a particular aspect of using the ElgatoKeyLight library, feel free to ask!
